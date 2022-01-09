a17c21c56c10cccd0805bd93388b61841620c889
AZ9tumas <77965895+AZ9tumas@users.noreply.github.com>
2022-01-09 07:20:34 +0400

    Update memory.c

--- src/memory.c
+++ src/memory.c
@@ -1,25 +1,36 @@
 #include <stdlib.h>
 
 #include "memory.h"
+#include "object.h"
 #include "vm.h"
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize){
-    if (newSize == 0){
-        free(pointer);
-        return NULL;
-    }
-    void* result = realloc(pointer, newSize);
-    if(result==NULL)exit(1);
-    return result;
+  if (newSize == 0){
+      free(pointer);
+      return NULL;
+  }
+  void* result = realloc(pointer, newSize);
+  if(result==NULL)exit(1);
+  return result;
 }
 
 static void freeObject(Obj* object) {
   switch (object->type) {
+    case OBJ_CLOSURE: {
+      ObjClosure* closure = (ObjClosure*)object;
+      FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
+      FREE(ObjClosure, object);
+      break;
+    }
     case OBJ_STRING: {
       ObjString* string = (ObjString*)object;
       FREE_ARRAY(char, string->chars, string->length + 1);
       FREE(ObjString, object);
       break;
+    }
+    case OBJ_UPVALUE: {
+        FREE(ObjClosure, object);
+        break;
     }
     case OBJ_FUNCTION:{
       ObjFunction* function = (ObjFunction*)object;
